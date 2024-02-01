966c5c38a0bfc58253cfddda06b3f25e8ffb5831
JosipTr <jtr.other13@gmail.com>
2024-02-01 14:52:13 +0100

    Initial commit

---
+++ src/main/java/com/example/imdb/authentication/controller/AuthController.java
@@ -0,0 +1,13 @@
+package com.example.imdb.authentication.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+	
+	@GetMapping("/")
+	public String getPublic() {
+		return "Public page";
+	}
+}
